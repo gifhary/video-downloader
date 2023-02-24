28c921c6cc19c9498f662708414dc9aed6891968
gifhary <gifhary.1997@gmail.com>
2023-02-24 23:49:30 +0700

    feat: summore

--- lib/module/insta_downloader/controller/insta_downloader_controller.dart
+++ lib/module/insta_downloader/controller/insta_downloader_controller.dart
@@ -47,14 +47,20 @@ class InstaDownloaderController extends GetxController
     super.onInit();
   }
 
+  @override
+  void onClose() async {
+    Get.find<AppNetworkClient>().removeHeader();
+    super.onClose();
+  }
+
   initDataFromWebview() async {
     loading = true;
     error = false;
     update();
     try {
       await webCtrl.setJavaScriptMode(JavaScriptMode.unrestricted);
-      await webCtrl.setUserAgent(
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)');
+      // await webCtrl.setUserAgent(
+      //     'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)');
       await webCtrl
           .loadRequest(Uri.parse('${_url.origin + _url.path}?__a=1&__d=dis'));
       await webCtrl.setNavigationDelegate(NavigationDelegate(
